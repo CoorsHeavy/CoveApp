a8a7db7485e51192d62bd1049511b3daaff39fca
CoorsHeavy <96hudson@gmail.com>
2016-05-14 19:25:12 -0700

    ugkj

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 //test for 4:51pm
 
 var fs = require('fs');
-var csv = require('csv');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var fs = require('fs');
-var csv = require('csv');
 var util = require('util');
 var stream = require('stream');
 var es = require('event-stream');
+var csv = require("fast-csv");
 var router = express.Router();
 
 /* GET home page. */
@@ -17,13 +17,18 @@ router.get('/query1', function(req, res, next) {
     var package = req.query.package;
     var startTime = req.query.startTime;
     var endTime = req.query.endTime;
-
-
-    var parser = csv.parse({delimiter: ','}, function(err, data){
-        console.log(data);
-    });
-
-    fs.createReadStream('/Users/kaylab/Pictures/app/public/csv/wifi_info/wifi_info_all_november.csv').pipe(parser);
+    var go = true;
+    var stream = fs.createReadStream('/Users/kaylab/Pictures/app/public/csv/wifi_info/wifi_info_all_november.csv');
+    csv
+        .fromStream(stream, {headers : true})
+        .on("data", function(data){
+            if(go)
+            console.log(data);
+            go = false;
+        })
+        .on("end", function(){
+            console.log("done");
+        });
     res.send(req.query.package);
 });
 
